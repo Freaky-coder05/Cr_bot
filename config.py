f09631f8cfee6221b2bf4645918765973ca9ced2
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-14 21:02:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 API_ID = os.getenv("API_ID", "21740783")
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
+MODEL_PATH = "/path/to/deepspeech-model.tflite"
+SCORER_PATH = "/path/to/deepspeech-model.scorer"
