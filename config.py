dabc1fc8582add66e46c2672eb9a8c79afbe70aa
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-15 10:26:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,13 @@ import os
 API_ID = os.getenv("API_ID", "21740783")
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
-MODEL_PATH = "/path/to/deepspeech-model.tflite"
-SCORER_PATH = "/path/to/deepspeech-model.scorer"
+
+# Directory to save and process files
+SAVE_DIR = './downloads'
+
+# FFmpeg path (if FFmpeg is not in your PATH, specify the full path here)
+FFMPEG_PATH = os.getenv("FFMPEG_PATH", "ffmpeg")
+
+# AI model settings
+MODEL_NAME = "facebook/bart-large-mnli"
+
