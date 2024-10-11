23aa131abcdddf20a3dc8284b1fa9115482830d5
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-10-11 16:10:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3a
 # Directory to save and process files
 SAVE_DIR = './downloads'
 
-# FFmpeg path (if FFmpeg is not in your PATH, specify the full path here)
-FFMPEG_PATH = os.getenv("FFMPEG_PATH", "ffmpeg")
-
 # AI model settings
 MODEL_NAME = "facebook/bart-large-mnli"
+
+FFMPEG_PATH = os.getenv("FFMPEG_PATH", "/usr/bin/ffmpeg")  # Default location of FFmpeg
 
