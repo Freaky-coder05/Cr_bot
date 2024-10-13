4c4c189bba130f4caf73488a6ea764b924237313
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-10-13 17:34:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = os.getenv("API_ID", "21740783")
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6610201435:AAHEv2YoM2ZEtlEdqjilv9mZGjT9Uzzrntw")
 
 # Directory to save and process files
 SAVE_DIR = './downloads'
